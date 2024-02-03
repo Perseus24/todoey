ed871202c6548332890af47411b475810416a033
Cy Jay Herrera <herreracyjay24@gmail.com>
2024-02-03 16:47:31 +0800

    feat(project): Finished the basic structure of Todoey

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:todoey/screens/tasks_screen.dart';
+import 'package:provider/provider.dart';
+
+import 'models/task.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => Data())
+      ],
+      child: MyApp(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,9 +23,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: TasksScreen(),
     );
   }
+}
+
+class Data with ChangeNotifier{
+
+  List<Task> _tasks  = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Leave family')  
+  ];
+
+  UnmodifiableListView<Task> get tasks{
+    return UnmodifiableListView(_tasks);
+  }
+
+  void addTasks(Task task){
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void updateTasks(Task task){
+    task.toggleDone();
+    notifyListeners();
+  }
+
+  void deleteTask(Task task){
+    _tasks.remove(task);
+    notifyListeners();
+  }
+
 }
 
 
